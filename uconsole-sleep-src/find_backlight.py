b8ebd61713310868d9f6ff32ce662f8aee4ed078
vxider <lb@vxider.com>
2026-05-06 16:39:04 +0800

    Remove legacy sleep control files

--- uconsole-sleep-src/find_backlight.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def find_backlight():
-    return "/sys/class/backlight/backlight@0"
-
-
-if __name__ == "__main__":
-    print(find_backlight())
--- uconsole-sleep-src/find_framebuffer.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def find_framebuffer():
-    return "/sys/class/graphics/fb0"
-
-
-if __name__ == "__main__":
-    print(find_framebuffer())
